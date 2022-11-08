44bc40fe21106a62a398506d2534396d373ed794
GG <477706421@qq.com>
2022-11-08 23:35:28 +0800

    add login post

--- src/main/java/com/yytech/mpcdsepc/controller/AccountController.java
+++ src/main/java/com/yytech/mpcdsepc/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.yytech.mpcdsepc.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -9,6 +11,13 @@ public class AccountController {
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String login(){
         return "login";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/account/login",method = RequestMethod.POST,produces = "application/json")
+    public JSONObject loginPOST(HttpRequestHandler httpRequestHandler){
+        JSONObject ret = new JSONObject();
+        return ret;
     }
 
 }
