b22949b0fa54a05ab48b15e3aad50a7bae995546
liu <1243885596@qq.com>
2022-11-09 13:12:38 +0800

    update mapper

---
+++ src/main/java/com/yytech/mpcdsepc/mapper/TubeMapper.java
@@ -0,0 +1,26 @@
+package com.yytech.mpcdsepc.mapper;
+
+import com.yytech.mpcdsepc.entity.Tube;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Mapper
+@Repository
+public interface TubeMapper {
+    List<Tube> getAllTubes();
+    Date getTubeCreateDateByID(Integer id);
+    Integer getTubeCreatorIDByID(Integer id);
+    Integer getTubeLastModifierIDByID(Integer id);
+
+    int updateTubeCreateDateByID(Tube tube);
+    int updateTubeCreatorByID(Tube tube);
+    int updateTubeLastModifierIDByID(Tube tube);
+
+    int addTube(Tube tube);
+
+    int delTubeByID(Integer id);
+
+}
