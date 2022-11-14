dea878564c47bdb58b53220929b04c3b95282b9f
Your Name <email@example.com>
2022-11-15 00:08:11 +0800

    "freemarker依赖注入"

---
+++ src/main/java/com/yytech/mpcdsepc/controller/api/document/ApiGenWordController.java
@@ -0,0 +1,21 @@
+package com.yytech.mpcdsepc.controller.api.document;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamSource;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/mpcdsepc/api/document")
+public class ApiGenWordController {
+
+
+
+}
