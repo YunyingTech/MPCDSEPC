626e93167b59254236de6b6d8cf15bec8d6bfe40
Doge <1429942@qq.com>
2022-11-14 15:18:39 +0800

    finished update mybatisPlus

--- src/main/java/com/yytech/mpcdsepc/controller/api/person/ApiPersonController.java
+++ src/main/java/com/yytech/mpcdsepc/controller/api/person/ApiPersonController.java
@@ -1,71 +1,87 @@
-//package com.yytech.mpcdsepc.controller.api.person;/**
-// * @Author: Lettle
-// * @Create: 2022-10-28 17:40
-// * @Description: Person 相关api
-// **/
-//
-//import com.yytech.mpcdsepc.entity.Person;
-//import com.yytech.mpcdsepc.result.Result;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.annotation.Resource;
-//
-///**
-// * @author Lettle
-// * Create by 2022/10/28 17:40
-// */
-//@RestController
-//@RequestMapping(value = "/mpcdsepc/api/person")
-//public class ApiPersonController {
-//    @Resource
-//    private PersonServiceCopy personService;
-//
-//    @GetMapping("getPersonByName")
-//    public Result getPersonByName(@RequestParam String name) {
-//        return Result.ok(personService.getPersonByName(name));
-//    }
-//
-//    @GetMapping("getPersonById")
-//    public Result getPersonById(@RequestParam String ID) {
-//        return Result.ok(personService.getPersonById(ID));
-//    }
-//
-//    @GetMapping("getPersonByTubeId")
-//    public Result getPersonByTubeId(@RequestParam("tubeId") int ID) {
-//        return Result.ok(personService.getPersonByTubeId(ID));
-//    }
-//
-//    @GetMapping("getPersonByAccountId")
-//    public Result getPersonByAccountId(@RequestParam("accountId") int ID) {
-//        return Result.ok(personService.getPersonByAccountId(ID));
-//    }
-//
-//    @PostMapping("insertPerson")
-//    public Result insertPerson(@RequestBody Person person) {
-//        if(personService.insertPerson(person)) {
-//            return Result.ok();
-//        } else {
-//            return Result.build(400,"ID重复");
-//        }
-//    }
-//
-//    @PutMapping("updatePerson")
-//    public Result updatePerson(@RequestParam Person person) {
-//        if(personService.updatePerson(person)) {
-//            return Result.ok();
-//        } else {
-//            return Result.build(400,"update failed");
-//        }
-//    }
-//
-//    @DeleteMapping("deletePersonById")
-//    public Result deletePersonById(@RequestParam int id) {
-//        if(personService.deletePersonById(id)) {
-//            return Result.ok();
-//        } else {
-//            return Result.build(400,"delete failed");
-//        }
-//    }
-//
-//
-//}
+package com.yytech.mpcdsepc.controller.api.person;/**
+ * @Author: Lettle
+ * @Create: 2022-10-28 17:40
+ * @Description: Person 相关api
+ **/
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.yytech.mpcdsepc.entity.Person;
+import com.yytech.mpcdsepc.result.Result;
+import com.yytech.mpcdsepc.service.PersonService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Lettle
+ * Create by 2022/10/28 17:40
+ */
+@RestController
+@RequestMapping(value = "/mpcdsepc/api/person")
+public class ApiPersonController {
+    @Resource
+    private PersonService personService;
+
+    @GetMapping("getPersonByName")
+    public Result getPersonByName(@RequestParam String name) {
+        LambdaQueryWrapper<Person> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(Person::getName,name);
+        List<Person> PersonList = personService.list(lambdaQueryWrapper);
+        return Result.ok(PersonList);
+    }
+
+    @GetMapping("getPersonById")
+    public Result getPersonById(@RequestParam String ID) {
+        Person person = personService.getById(ID);
+        return Result.ok(person);
+    }
+
+    @GetMapping("getPersonByTubeId")
+    public Result getPersonByTubeId(@RequestParam("tubeId") int ID) {
+        LambdaQueryWrapper<Person> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(Person::getTubeId,ID);
+        List<Person> PersonList = personService.list(lambdaQueryWrapper);
+        return Result.ok(PersonList);
+    }
+
+    @GetMapping("getPersonByAccountId")
+    public Result getPersonByAccountId(@RequestParam("accountId") int ID) {
+        LambdaQueryWrapper<Person> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(Person::getAccountId,ID);
+        List<Person> PersonList = personService.list(lambdaQueryWrapper);
+        return Result.ok(PersonList);
+    }
+
+    @PostMapping("insertPerson")
+    public Result insertPerson(@RequestBody Person person) {
+        boolean flag = personService.save(person);
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.build(400,"ID重复");
+        }
+    }
+
+    @PutMapping("updatePerson")
+    public Result updatePerson(@RequestParam Person person) {
+        boolean flag = personService.update(person,null);
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.build(400,"update failed");
+        }
+    }
+
+    @DeleteMapping("deletePersonById")
+    public Result deletePersonById(@RequestParam int id) {
+        boolean flag = personService.removeById(id);
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.build(400,"delete failed");
+        }
+    }
+
+
+}
