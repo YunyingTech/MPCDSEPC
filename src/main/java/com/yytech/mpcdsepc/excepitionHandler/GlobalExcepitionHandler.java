ee7b37b7aa2b67bbc8492b70b7936b600b1962e6
Doge <1429942@qq.com>
2022-11-23 10:41:02 +0800

    =-=

--- src/main/java/com/yytech/mpcdsepc/excepitionHandler/GlobalExcepitionHandler.java
+++ src/main/java/com/yytech/mpcdsepc/excepitionHandler/GlobalExcepitionHandler.java
@@ -1,16 +1,16 @@
-//package com.yytech.mpcdsepc.excepitionHandler;
-//
-//import com.yytech.mpcdsepc.result.Result;
-//import com.yytech.mpcdsepc.utils.StatusCodeUtil;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//import java.sql.SQLIntegrityConstraintViolationException;
-//
-//@RestControllerAdvice
-//public class GlobalExcepitionHandler {
-//    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-//    public Result DuplicateHandler(){
-//        return Result.build(StatusCodeUtil.DuplicateKey,"当前输入的账号已存在！");
-//    }
-//}
+package com.yytech.mpcdsepc.excepitionHandler;
+
+import com.yytech.mpcdsepc.result.Result;
+import com.yytech.mpcdsepc.utils.StatusCodeUtil;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExcepitionHandler {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Result DuplicateHandler(){
+        return Result.build(StatusCodeUtil.DuplicateKey,"新增数据异常");
+    }
+}
